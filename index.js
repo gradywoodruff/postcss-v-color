172d8d43eba3d66911a8abb1c5c365448607ce80
gradywoodruff <jgradywoodruffiv@gmail.com>
2022-03-23 19:41:16 -0400

    Remove editorconfig

--- index.js
+++ index.js
@@ -2,13 +2,15 @@
  * @type {import('postcss').PluginCreator}
  */
 
-const parseGroup = require("./lib/parsers/parse-group");
-const parseAttrs = require("./lib/parsers/parse-attrs");
+const parseGroup = require("./lib/parsers/parse-group")
+const parseAttrs = require("./lib/parsers/parse-attrs")
 
 module.exports = (opts = {}) => {
-  const dark = opts.darkSelector || `[data-color-mode="dark"]`;
-  const light = `:not([data-color-mode="${opts.darkSelector || "light"}"])`;
-  const colors = new Map();
+  const dark = opts.darkSelector || `[data-color-mode="dark"]`
+  const light = `:not([data-color-mode="${
+    opts.darkSelector || "light"
+  }"])`
+  const colors = new Map()
 
   return {
     postcssPlugin: "postcss-darkmode",
@@ -25,91 +27,40 @@ module.exports = (opts = {}) => {
     //   console.log(postcss);
     // },
 
-    AtRuleExit(atrule) {
-      if (atrule.name !== "v-color") return;
-
-      // let color = atrule.params.match(/(.*):? ?\(([^)]+)\)/)[1].trim();
-      // let params = Object.fromEntries(
-      //   atrule.params
-      //     .match(/\(([^)]+)\)/)[1]
-      //     .split(",")
-      //     .map((arg) => arg.trim())
-      //     .map((val) => val.split(":").map((val) => val.trim()))
-      // );
-
-      const params = parseAttrs(parseGroup(atrule.params)[0]).map((group) =>
-        group.map((item, i) =>
-          i === 1
-            ? Object.fromEntries(
-                new Set(
-                  item
-                    .trim()
-                    .split(/,/g)
-                    .map((line) => line.split(":").map((prop) => prop.trim()))
-                )
-              )
-            : item.trim()
+    AtRule: {
+      "v-color": atrule => {
+        const params = parseAttrs(parseGroup(atrule.params)[0]).map(
+          group =>
+            group.map((item, i) =>
+              i === 1
+                ? Object.fromEntries(
+                    new Set(
+                      item
+                        .trim()
+                        .split(/,/g)
+                        .map(line =>
+                          line.split(":").map(prop => prop.trim())
+                        )
+                    )
+                  )
+                : item.trim()
+            )
         )
-      );
 
-      for (const [color, settings] of params) {
-        colors.set(color, settings);
-      }
-
-      console.log("******************");
-      console.log("colors");
-      console.log(colors);
-    },
-
-    /*
-    Declaration (decl, postcss) {
-      // The faster way to find Declaration node
-    }
-    */
-
-    /*
-    Declaration: {
-      color: (decl, postcss) {
-        // The fastest way find Declaration node if you know property name
+        for (const [color, settings] of params) {
+          colors.set(color, { rule: atrule, settings })
+        }
+        console.log("******************")
+        console.log("colors")
+        console.log(colors)
+        atrule.each(node => {
+          console.log("******************")
+          console.log("node")
+          console.log(node)
+        })
       }
     }
-    */
-  };
-};
+  }
+}
 
-module.exports.postcss = true;
-
-// const postcss = require("postcss")
-// const parseValue = require("postcss-value-parser")
-
-// module.exports = postcss.plugin("postcss-darkmode", (opts = {}) => {
-//   opts.functionName = opts.functionName || "darkmode"
-
-//   return (css) => {
-//     css.walkDecls((decl) => {
-//       let parsedValue = parseValue(decl.value)
-
-//       decl.value = parsedValue
-//         .walk((node) => {
-//           if (node.value !== opts.functionName) return
-
-//           let params = [],
-//             options = {}
-//           if (node.type === "function") {
-//             params = node.nodes
-//               .filter(({ type }) => type === "word")
-//               .map(({ value }) => value)
-
-//             node.type = "word"
-//           }
-
-//           if (params[0]) options.luminosity = params[0]
-//           if (params[1]) options.hue = params[1]
-//           if (opts.format) options.format = opts.format
-
-//           node.value = randomColor(options)
-//         })
-//         .toString()
-//     })
-//   }
-// })
+module.exports.postcss = true
--- index.test.js
+++ index.test.js
@@ -1,13 +1,13 @@
-const postcss = require("postcss");
+const postcss = require("postcss")
 
-const plugin = require("./");
+const plugin = require("./")
 
 async function run(input, output, opts = {}) {
   let result = await postcss([plugin(opts)]).process(input, {
-    from: undefined,
-  });
-  expect(result.css).toEqual(output);
-  expect(result.warnings()).toHaveLength(0);
+    from: undefined
+  })
+  expect(result.css).toEqual(output)
+  expect(result.warnings()).toHaveLength(0)
 }
 
 // Write tests here
@@ -15,42 +15,35 @@ it("does something", async () => {
   await run(
     `
     @v-color (
-      text-background ( 
-        light: var(--color-white), 
-        light_high_contrast: var(--color-white),
-        dark: var(--color-black),
-        dark_high_contrast: var(--color-white),
-        dark_dimmed: var(--color-grey-dark)
+      background ( 
+        light: var(--color-white),
+        dark: var(--color-black)
       ),
       text-headline ( 
-        light: var(--color-grey-dark), 
-        light_high_contrast: var(--color-black),
-        dark: var(--color-grey-light),
-        dark_high_contrast: var(--color-white),
-        dark_dimmed: var(--color-grey-medium)
+        light: var(--color-grey-dark),
+        dark: var(--color-grey-light)
       ),
       text-highlight ( 
-        light: var(--color-grey-dark), 
-        light_high_contrast: var(--color-black),
-        dark: var(--color-grey-light),
-        dark_high_contrast: var(--color-white),
-        dark_dimmed: var(--color-grey-medium)
+        light: var(--color-grey-dark),
+        dark: var(--color-grey-light)
       )
     );
   `,
     `
     :root {
-      --color-highlight-text: #000;
-      --color-normal-text: #000;
+      --color-background: var(--color-white);
+      --color-text-headline: var(--color-grey-dark);
+      --color-text-highlight: var(--color-grey-dark);
     }
 
     @media (prefers-color-scheme: dark) {
       :root {
-        --color-highlight-text: #fff;
-        --color-normal-text: #fff;
+        --color-background: var(--color-black);
+        --color-text-headline: var(--color-grey-light);
+        --color-text-highlight: var(--color-grey-light);
       }
     }
   `,
     {}
-  );
-});
+  )
+})

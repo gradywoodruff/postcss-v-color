2978edc8720628b1afd270e6fc5714b951105a13
gradywoodruff <jgradywoodruffiv@gmail.com>
2022-03-23 18:15:19 -0400

    Set up

--- index.js
+++ index.js
@@ -1,16 +1,65 @@
 /**
  * @type {import('postcss').PluginCreator}
  */
+
+const parseGroup = require("./lib/parsers/parse-group");
+const parseAttrs = require("./lib/parsers/parse-attrs");
+
 module.exports = (opts = {}) => {
-  // Work with options here
+  const dark = opts.darkSelector || `[data-color-mode="dark"]`;
+  const light = `:not([data-color-mode="${opts.darkSelector || "light"}"])`;
+  const colors = new Map();
 
   return {
-    postcssPlugin: 'postcss-darkmode',
-    /*
-    Root (root, postcss) {
-      // Transform CSS AST here
-    }
-    */
+    postcssPlugin: "postcss-darkmode",
+    // Root(root, postcss) {
+    //   const args = root.nodes[0].params
+    //     .match(/\(([^)]+)\)/)[1]
+    //     .split(",")
+    //     .map((arg) => arg.trim());
+
+    //   for (const arg of args) {
+    //     const [key, value] = arg.split(":").map((arg) => arg.trim());
+    //   }
+    //   console.log("postcss");
+    //   console.log(postcss);
+    // },
+
+    AtRuleExit(atrule) {
+      if (atrule.name !== "v-color") return;
+
+      // let color = atrule.params.match(/(.*):? ?\(([^)]+)\)/)[1].trim();
+      // let params = Object.fromEntries(
+      //   atrule.params
+      //     .match(/\(([^)]+)\)/)[1]
+      //     .split(",")
+      //     .map((arg) => arg.trim())
+      //     .map((val) => val.split(":").map((val) => val.trim()))
+      // );
+
+      const params = parseAttrs(parseGroup(atrule.params)[0]).map((group) =>
+        group.map((item, i) =>
+          i === 1
+            ? Object.fromEntries(
+                new Set(
+                  item
+                    .trim()
+                    .split(/,/g)
+                    .map((line) => line.split(":").map((prop) => prop.trim()))
+                )
+              )
+            : item.trim()
+        )
+      );
+
+      for (const [color, settings] of params) {
+        colors.set(color, settings);
+      }
+
+      console.log("******************");
+      console.log("colors");
+      console.log(colors);
+    },
 
     /*
     Declaration (decl, postcss) {
@@ -25,7 +74,42 @@ module.exports = (opts = {}) => {
       }
     }
     */
-  }
-}
+  };
+};
 
-module.exports.postcss = true
+module.exports.postcss = true;
+
+// const postcss = require("postcss")
+// const parseValue = require("postcss-value-parser")
+
+// module.exports = postcss.plugin("postcss-darkmode", (opts = {}) => {
+//   opts.functionName = opts.functionName || "darkmode"
+
+//   return (css) => {
+//     css.walkDecls((decl) => {
+//       let parsedValue = parseValue(decl.value)
+
+//       decl.value = parsedValue
+//         .walk((node) => {
+//           if (node.value !== opts.functionName) return
+
+//           let params = [],
+//             options = {}
+//           if (node.type === "function") {
+//             params = node.nodes
+//               .filter(({ type }) => type === "word")
+//               .map(({ value }) => value)
+
+//             node.type = "word"
+//           }
+
+//           if (params[0]) options.luminosity = params[0]
+//           if (params[1]) options.hue = params[1]
+//           if (opts.format) options.format = opts.format
+
+//           node.value = randomColor(options)
+//         })
+//         .toString()
+//     })
+//   }
+// })
--- index.test.js
+++ index.test.js
@@ -1,17 +1,56 @@
-const postcss = require('postcss')
+const postcss = require("postcss");
 
-const plugin = require('./')
+const plugin = require("./");
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
+async function run(input, output, opts = {}) {
+  let result = await postcss([plugin(opts)]).process(input, {
+    from: undefined,
+  });
+  expect(result.css).toEqual(output);
+  expect(result.warnings()).toHaveLength(0);
 }
 
-/* Write tests here
+// Write tests here
+it("does something", async () => {
+  await run(
+    `
+    @v-color (
+      text-background ( 
+        light: var(--color-white), 
+        light_high_contrast: var(--color-white),
+        dark: var(--color-black),
+        dark_high_contrast: var(--color-white),
+        dark_dimmed: var(--color-grey-dark)
+      ),
+      text-headline ( 
+        light: var(--color-grey-dark), 
+        light_high_contrast: var(--color-black),
+        dark: var(--color-grey-light),
+        dark_high_contrast: var(--color-white),
+        dark_dimmed: var(--color-grey-medium)
+      ),
+      text-highlight ( 
+        light: var(--color-grey-dark), 
+        light_high_contrast: var(--color-black),
+        dark: var(--color-grey-light),
+        dark_high_contrast: var(--color-white),
+        dark_dimmed: var(--color-grey-medium)
+      )
+    );
+  `,
+    `
+    :root {
+      --color-highlight-text: #000;
+      --color-normal-text: #000;
+    }
 
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
-})
-
-*/
+    @media (prefers-color-scheme: dark) {
+      :root {
+        --color-highlight-text: #fff;
+        --color-normal-text: #fff;
+      }
+    }
+  `,
+    {}
+  );
+});

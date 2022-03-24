f3dd9635dbac1323fef3463f6692ae3b0f53d5d8
gradywoodruff <jgradywoodruffiv@gmail.com>
2022-03-24 09:41:22 -0400

    Update eslint and testing

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const parseAttrs = require("./lib/parsers/parse-attrs")
 module.exports = (opts = {}) => {
   // const dark = opts.darkSelector || `[data-color-mode="dark"]`
   // const light = `:not([data-color-mode="${
-  //   opts.darkSelector || "light"
+  //   opts.lightSelector || "light"
   // }"])`
   const colors = new Map()
 
@@ -18,26 +18,32 @@ module.exports = (opts = {}) => {
     postcssPlugin: "postcss-v-color",
     AtRule: {
       "v-color": (atRule, { result }) => {
-        const params = parseAttrs(parseGroup(atRule.params)[0]).map(
-          group =>
-            group.map((item, i) =>
-              i === 1
-                ? Object.fromEntries(
-                    new Set(
-                      item
-                        .trim()
-                        .split(/,/g)
-                        .map(line =>
-                          line.split(":").map(prop => prop.trim())
-                        )
-                    )
+        let params = parseGroup(atRule.params)[0]
+        if (!params) {
+          atRule.warn(result, "No color group found")
+          atRule.remove()
+          return
+        }
+        params = parseAttrs(params)
+        params = params.map(group =>
+          group.map((item, i) =>
+            i === 1
+              ? Object.fromEntries(
+                  new Set(
+                    item
+                      .trim()
+                      .split(/,/g)
+                      .map(line =>
+                        line.split(":").map(prop => prop.trim())
+                      )
                   )
-                : item.trim()
-            )
+                )
+              : item.trim()
+          )
         )
 
         for (const [color, settings] of params) {
-          colors.set(color, settings)
+          colors.set(color, { settings, opts })
         }
 
         const lightEls = []
@@ -46,23 +52,19 @@ module.exports = (opts = {}) => {
           Object.fromEntries(colors)
         )) {
           const colorName = `--color-${color}`
-          lightEls.push(`${colorName}:${values.light};`)
-          darkEls.push(`${colorName}:${values.dark};`)
+          lightEls.push(`${colorName}:${values.settings.light};`)
+          darkEls.push(`${colorName}:${values.settings.dark};`)
         }
 
-        try {
-          atRule.parent.insertBefore(
-            atRule,
-            `:root {${lightEls.join(
-              ""
-            )}}@media (prefers-color-scheme: dark) {:root {${darkEls.join(
-              ""
-            )}}}`
-          )
-          atRule.remove()
-        } catch (error) {
-          atRule.warn(result, error)
-        }
+        atRule.parent.insertBefore(
+          atRule,
+          `:root {${lightEls.join(
+            ""
+          )}}@media (prefers-color-scheme: dark) {:root {${darkEls.join(
+            ""
+          )}}}`
+        )
+        atRule.remove()
       }
     }
   }
--- index.test.js
+++ index.test.js
@@ -7,34 +7,79 @@
 
 const postcss = require("postcss")
 const plugin = require("./")
+const expect = require("expect")
+const TESTS = {
+  success: {
+    idealFormatting: [
+      `
+      @v-color (
+        background ( 
+          light: var(--color-white),
+          dark: var(--color-black)
+        ),
+        text-headline ( 
+          light: var(--color-grey-dark),
+          dark: var(--color-grey-light)
+        ),
+        text-highlight ( 
+          light: var(--color-grey-dark),
+          dark: var(--color-grey-light)
+        )
+      );`,
+      ":root {--color-background:var(--color-white);--color-text-headline:var(--color-grey-dark);--color-text-highlight:var(--color-grey-dark);}@media (prefers-color-scheme: dark) {:root {--color-background:var(--color-black);--color-text-headline:var(--color-grey-light);--color-text-highlight:var(--color-grey-light);}}"
+    ],
+    oneLiner: [
+      `
+      @v-color(background(light:var(--color-white),dark:var(--color-black)),text-headline(light:var(--color-grey-dark),dark: var(--color-grey-light)),text-highlight(light:var(--color-grey-dark),dark: var(--color-grey-light)));`,
+      ":root {--color-background:var(--color-white);--color-text-headline:var(--color-grey-dark);--color-text-highlight:var(--color-grey-dark);}@media (prefers-color-scheme: dark) {:root {--color-background:var(--color-black);--color-text-headline:var(--color-grey-light);--color-text-highlight:var(--color-grey-light);}}"
+    ],
+    linearFormattingWithSpaces: [
+      `
+      @v-color (
+        background ( light: var(--color-white), dark: var(--color-black) ),
+        text-headline ( light: var(--color-grey-dark), dark: var(--color-grey-light) ),
+        text-highlight ( light: var(--color-grey-dark), dark: var(--color-grey-light) )
+      );`,
+      ":root {--color-background:var(--color-white);--color-text-headline:var(--color-grey-dark);--color-text-highlight:var(--color-grey-dark);}@media (prefers-color-scheme: dark) {:root {--color-background:var(--color-black);--color-text-headline:var(--color-grey-light);--color-text-highlight:var(--color-grey-light);}}"
+    ],
+    linearFormattingWithoutSpaces: [
+      `
+      @v-color(
+        background (light:var(--color-white),dark:var(--color-black)),
+        text-headline (light:var(--color-grey-dark),dark: var(--color-grey-light)),
+        text-highlight (light:var(--color-grey-dark),dark: var(--color-grey-light))
+      );`,
+      ":root {--color-background:var(--color-white);--color-text-headline:var(--color-grey-dark);--color-text-highlight:var(--color-grey-dark);}@media (prefers-color-scheme: dark) {:root {--color-background:var(--color-black);--color-text-headline:var(--color-grey-light);--color-text-highlight:var(--color-grey-light);}}"
+    ]
+  },
+  failure: {
+    noParentheses: [
+      `
+      @v-color background;`,
+      "",
+      { expectEmpty: true }
+    ]
+  }
+}
 
 async function run(input, output, opts = {}) {
   let result = await postcss([plugin(opts)]).process(input, {
     from: undefined
   })
   expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
+  if (!opts.expectEmpty) {
+    expect(result.warnings()).toHaveLength(0)
+  }
 }
 
-// Write tests here
-it("does something", async () => {
-  await run(
-    `
-    @v-color (
-      background ( 
-        light: var(--color-white),
-        dark: var(--color-black)
-      ),
-      text-headline ( 
-        light: var(--color-grey-dark),
-        dark: var(--color-grey-light)
-      ),
-      text-highlight ( 
-        light: var(--color-grey-dark),
-        dark: var(--color-grey-light)
-      )
-    );`,
-    ":root {--color-background:var(--color-white);--color-text-headline:var(--color-grey-dark);--color-text-highlight:var(--color-grey-dark);}@media (prefers-color-scheme: dark) {:root {--color-background:var(--color-black);--color-text-headline:var(--color-grey-light);--color-text-highlight:var(--color-grey-light);}}",
-    {}
-  )
-})
+for (const [type, tests] of Object.entries(TESTS)) {
+  for (let [name, [input, output, opts]] of Object.entries(tests)) {
+    name = name.replace(/([A-Z])/g, " $1")
+    name = name.charAt(0).toUpperCase() + name.slice(1)
+
+    // eslint-disable-next-line no-undef
+    it(`${type.toUpperCase()}: ${name}`, async () => {
+      await run(input, output, opts || {})
+    })
+  }
+}
